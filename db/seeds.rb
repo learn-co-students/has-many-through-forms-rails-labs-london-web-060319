97dc5ebb4c9853b1f249992aeb25ef8c33027dc9
Scott <galejscott@gmail.com>
2019-07-03 10:24:46 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    groove = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+    bob = User.create(username: 'CoolGuy5')
+    rob = User.create(username: 'CoolGuy6')
+    comment1 = Comment.create(content: "Great Post!", user_id: 1, post_id: 1)
+    comment2 = Comment.create(content: "Rubbish Post!", user_id: 1, post: 1)
+    comment3 = Comment.create(content: "Ok Post!", user_id: 2, post: 1)
+    
+    comment1 = groove.comments.build(content: "Great Post!", user: bob)
+    comment2 = groove.comments.build(content: "Rubbish Post!", user: bob)
+    comment3 = groove.comments.build(content: "Ok Post!", user: rob)
+    groove.save
